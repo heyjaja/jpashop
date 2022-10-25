775635d29498d3139a712e01550321d765719958
heyjaja <imjaja09@gmail.com>
2022-10-25 23:05:16 +0900

    JPA 활용 1 완료

--- src/main/java/com/jpabook/jpashop/service/OrderService.java
+++ src/main/java/com/jpabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import com.jpabook.jpashop.domain.OrderItem;
 import com.jpabook.jpashop.repository.ItemRepository;
 import com.jpabook.jpashop.repository.MemberRepository;
 import com.jpabook.jpashop.repository.OrderRepository;
+import com.jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -63,7 +64,7 @@ public class OrderService {
     /**
      * 주문 검색
      */
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByCriteria(orderSearch);
+    }
 }
